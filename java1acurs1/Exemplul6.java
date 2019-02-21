198dc7f846d125c3ba373dc640d694d0dcae42ca
cristina1495 <cristina.n232@gmail.com>
2019-02-21 21:40:41 +0200

    Curs 1

---
+++ java1acurs1/Exemplul6.java
@@ -0,0 +1,17 @@
+public class Exemplul6 {
+    public static void main (String [] args) {
+        boolean b1 = true;
+        
+        boolean b2 = !b1;
+        
+        boolean b3 = !(3 <4 );
+        
+        boolean b4 = b1 & b2;
+        boolean b5 = b1 && b2;
+        
+        boolean b6 = b1 | b2;
+        boolean b7 = b1 || b2;
+        
+        
+    }
+}
---
+++ java1acurs1/Exemplul7.java
@@ -0,0 +1,14 @@
+/**
+ * exemplul 7
+ */
+public class Exemplul7 {
+    /*
+     * Aici scriu
+     * un comentariu
+     */
+    public static void main(String [] args) {
+        String x = 3 > 6 ? "Hello" : "World";
+        
+        System.out.println(x);
+    }
+}
